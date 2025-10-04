27184b5e7415f2d6c33e0a960d3d5c627bc9b1e3
Andrew Michienzi <andrew@grcmc.org>
2025-10-04 13:26:59 -0400

    handling in main

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @export var candidate_manager: CandidateManager
+@export var worker_manager: WorkerManager
 @export var potential_income_stream_manager: PotentialIncomeStreamsManager
 @export var active_income_streams_manager: ActiveIncomeStreamsManager
 
@@ -14,3 +15,10 @@ func _ready() -> void:
 func _on_button_button_up() -> void:
 	
 	pass # Replace with function body.
+
+
+func _on_candidate_manager_worker_hired(worker: Worker) -> void:
+	worker_manager.hire_worker(worker)
+
+func _on_potential_income_stream_manager_income_stream_accepted(income_stream: IncomeStream) -> void:
+	active_income_streams_manager.accept_income_stream(income_stream)
