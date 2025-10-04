1e3df2cc807522bdeaca66f2a81c79fb47490fb2
Andrew Michienzi <andrew@grcmc.org>
2025-10-04 02:04:05 -0400

    hiring workers

--- desk/room.gd
+++ desk/room.gd
@@ -4,6 +4,8 @@ class_name Room
 @export var candidate_manager: CandidateManager
 @export var worker_manager: WorkerManager
 @export var resumes: Resumes
+@export var phone: Phone
 
 func _ready() -> void:
 	resumes.set_candidate_manager(candidate_manager)
+	phone.set_worker_manager(worker_manager)
