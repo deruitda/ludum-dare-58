9b1e1d8a57171f4b82c6a540de96fc42f0509d3e
Andrew Michienzi <andrew@grcmc.org>
2025-10-04 20:53:48 -0400

    wip

--- ui/capacity_row.gd
+++ ui/capacity_row.gd
@@ -1,12 +1,26 @@
 extends Control
 class_name CapacityRow
+signal capacity_button_pressed(capacity: Capacity)
 
-func set_income_stream(income_stream: IncomeStream) -> void:
+@onready var income_stream: IncomeStream
+
+func set_income_stream(new_income_stream: IncomeStream) -> void:
+	income_stream = new_income_stream
+	refresh_capacity_state()
+
+func refresh_capacity_state() -> void:
 	var length = len(get_children())
 	for i in length:
 		if income_stream.capacity > i:
-			get_children().get(i).visible = true
+			var button = get_children().get(i) as Control
+			button.visible = true
+			button.set_capacity(income_stream.get_capacity(i))
+			button.on_capacity_button_press.connect(_on_capacity_button_on_capacity_button_press)
 		else:
 			get_children().get(i).visible = false
 
-		
+
+func _on_capacity_button_on_capacity_button_press(capacity: Capacity) -> void:
+	print("capacity row")
+	capacity_button_pressed.emit(capacity)
+	pass # Replace with function body.
