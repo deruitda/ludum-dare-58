74823b3f042c3f19a5e1e96c69160a37074b878c
Andrew Michienzi <andrew@grcmc.org>
2025-10-06 18:20:17 -0400

    tweaking job ui

--- ui/income_stream_view.gd
+++ ui/income_stream_view.gd
@@ -5,8 +5,9 @@ class_name IncomeStreamView
 
 @onready var name_label: Label = $NameLabel
 @onready var description_label: Label = $DescriptionLabel
+@onready var money_per_week_value: StatLabel = $PerWeekControl/MoneyPerWeekValue
+@onready var respect_label: StatLabel = $PerWeekControl2/RespectLabel
 
-@onready var per_week_cost_view: CostView = $PerWeekControl/PerWeekCostView
 @onready var completion_reward_control_view: CostView = $CompletionRewardControl/CompletionRewardControlView
 
 @onready var weeks_left_value: StatLabel = $HBoxContainer/WeeksLeftValue
@@ -19,7 +20,8 @@ func set_income_stream(new_income_stream: IncomeStream):
 	name_label.text = income_stream.income_stream_name
 	description_label.text = income_stream.description
 	weeks_left_value.set_value(income_stream.get_weeks_left())
-	per_week_cost_view.set_cost(income_stream.weekly_return_cost)
+	money_per_week_value.set_value(income_stream.weekly_return_cost.cost)
+	respect_label.set_value(income_stream.weekly_return_cost.respect)
 	completion_reward_control_view.set_cost(income_stream.completed_return_cost)
 	percentage_label.set_percentage(income_stream.percent_chance_of_event)
 	
